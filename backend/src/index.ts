ae8f9fc59fc1619ba82d0c1db204945bf2401ffa
idomyhomework <alex.scheglov25@gmail.com>
2025-11-08 14:23:21 +0100

    I added express and a simple request from the backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,3 +1,13 @@
-console.log('hello, World!')
+import express from 'express'
+
+
+const expressApp = express()
+expressApp.get('/ping', (req, res) =>{
+    res.send('pong')
+})
+
+expressApp.listen(8080, ()=>{
+    console.info('Listening at http://localhost:8080')
+})
  
 // need to change the ts.config

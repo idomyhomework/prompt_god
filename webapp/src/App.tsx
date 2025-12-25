32140dede765c2182331564df66cd7005fc62f62
idomyhomework <alex.scheglov25@gmail.com>
2025-12-25 14:40:26 +0100

    Initial commit

---
+++ webapp/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-slate-900">
+      <h1 className="text-3xl font-bold text-white">
+        Tailwind v3 with pnpm 🚀
+      </h1>
+    </div>
+  );
+}
+
+export default App;
